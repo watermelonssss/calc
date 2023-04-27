6abc3bfbc50fd64202458367cbddfc23eb21a332
watermelonssss <118621322+watermelonssss@users.noreply.github.com>
2023-04-27 12:45:16 -0400

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add() {
+  float num1;
+  float num2;
+  float sum;
+  printf("Enter 1st number: ");
+  scanf("%f", &num1);
+  printf("Enter 2nd number: ");
+  scanf("%f", &num2);
+  sum = num1 + num2
+  printf("\nSum: ");
+int main() {
+  add();
+  return 0;
+}
